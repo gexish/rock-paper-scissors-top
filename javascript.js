bd5db2fcf634ec5701039f5bd355b5ddd4f5d55a
gexish <gabrielsx2701@gmail.com>
2026-04-23 18:34:45 +0900

    Write code for getting human choice

--- javascript.js
+++ javascript.js
@@ -8,3 +8,21 @@ function computerChoice() {
         return "scissors";
     }
 }
+
+function humanChoice() {
+    let getHumanChoice = prompt("Rock, paper or scissors? Make your choice.");
+    if (getHumanChoice === null) {
+        alert("Oh, are you going to run away?");
+        return;
+    }
+    if (getHumanChoice.toLowerCase()  === "rock") {
+        return "rock";
+    } else if (getHumanChoice.toLowerCase()  === "paper") {
+        return "paper";
+    } else if (getHumanChoice.toLowerCase()  === "scissors") {
+        return "scissors";
+    } else {
+        alert("Please input one of the valid alternatives.");
+        return;
+    }
+}
