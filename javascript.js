2a15cd0f4f41d40a764b9926094d74c4fa458ac1
gexish <gabrielsx2701@gmail.com>
2026-04-23 18:33:34 +0900

    Write code for random computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function computerChoice() {
+    let getComputerChoice =  Math.random();
+    if (getComputerChoice <= 1/3) {
+        return "rock";
+    } else if (getComputerChoice <= 2/3) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
